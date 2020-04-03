8000895a4b1bd8ee43256b3125b4cc5b63897e14
Jonas Blanc <jonas.blanc@epfl.ch>
2020-04-03 21:59:56 +0200

    Add first version of Grid

--- src/Grid.java
+++ src/Grid.java
@@ -1,8 +1,38 @@
 
 public final class Grid {
 
-    public Grid() {
-        // TODO Auto-generated constructor stub
+    private Cell [][] grid;
+    private final int sizeX;
+    private final int sizeY;
+    
+    public Grid(int sizeX, int sizeY) {
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        initGrid();
     }
 
+    private final void initGrid() {
+        grid = new Cell[sizeX][sizeY];
+        for(int x = 0; x<sizeX; ++x) {
+            for(int y = 0; y < sizeY; ++y) {
+                grid[x][y] = new Cell(x,y);
+            }
+        }
+    }
+    
+    public Cell getCell(int x, int y) {
+        return grid[x][y];
+    }
+    
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(int y = sizeY-1; y >= 0; --y) {
+            for(int x = 0; x<sizeX; ++x) {
+                sb.append(grid[x][y].toString());
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+        
+    }
 }

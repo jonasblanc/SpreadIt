c16455431ab7696e6c8cb98d6401880af4a42cd5
Jonas Blanc <jonas.blanc@epfl.ch>
2020-04-03 21:16:31 +0200

    Add basis for cell

--- src/Cell.java
+++ src/Cell.java
@@ -1,8 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public final class Cell {
+    private final int x;
+    private final int y;
+    
+    private List<MovableEntity> movableEntities = new ArrayList<>();
+    
+    public Cell(int posX, int posY) {
+        x = posX;
+        y = posY;
+    }
 
-    public Cell() {
-        // TODO Auto-generated constructor stub
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+    public void addEntity(MovableEntity e) {
+        movableEntities.add(e);
     }
 
 }
--- src/HealtyEntity.java
+++ src/HealtyEntity.java
@@ -1,16 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class HealtyEntity extends MovableEntity {
 
-    private final int x;
-    private final int y;
     
-    private List<MovableEntity> movableEntities = new ArrayList<>();
-    
-    public HealtyEntity(int posX, int posY) {
-        x = posX;
-        y = posY;
+    public HealtyEntity() {
+       
     }
-
+    
 }

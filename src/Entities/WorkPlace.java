27c1ddcf11d5906439d4524aada85ffff7bd0e36
Squalene <gerard.depardieu@mail.com>
2020-04-04 16:39:55 +0200

    Add workPlace

---
+++ src/Entities/WorkPlace.java
@@ -0,0 +1,32 @@
+package Entities;
+
+import GameEnvironment.Grid;
+
+public class WorkPlace extends Buildings{
+
+    /**
+     * @param x : x position
+     * @param y : y position
+     * @param area : grid the workPlace is in
+     * @param capacity
+     */
+    public WorkPlace(int x, int y, Grid area, int capacity) {
+        super(x, y, area, capacity);
+    }
+
+    @Override
+    public boolean takeCellSpace() {
+        return isFull();
+    }
+
+    @Override
+    public String toString() {
+        return "W";
+    }
+
+
+    @Override
+    public void update() { 
+    }
+
+}
